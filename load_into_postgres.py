ea3cd350b99cc9c9796da682d064e05d547dff08
Shah <aashay.as99@gmail.com>
2023-03-16 18:54:26 -0700

    Load data from ECU

--- load_into_postgres.py
+++ load_into_postgres.py
@@ -1,10 +1,11 @@
 import click
 import psycopg as pg
 import threading
+from udpRead import UdpReader
 
 @click.command()
 @click.option('--dbname', default='postgres', help="Name of database")
-@click.option('--password', prompt=True, hide_input=True)
+@click.option('--password', default='postgres', prompt=True, hide_input=True)
 @click.option('--user', default='postgres', help="Postgres Username")
 @click.option('--ecu-ip', help="IP Address of the ECU")
 @click.option('--db-host', default='localhost', help="Host of database. Can be IP or Domain")
@@ -13,12 +14,27 @@ def start_loading(dbname, password, user, ecu_ip, db_host):
     thread = threading.Thread(target=udpReader.serialHandler)
     thread.start()
     while True:
+        try:
+            data = udpReader.getData(f"tc;sAll;")
+            if (data is not None):
+                data = str(data, encoding='utf-8')
+                tc, sAll, sol, *extra = data.split(';')
+
+                print("TC: ", tc)
+                print("Solenoid: ", sAll)
+                """print("RTT: ", time.time_ns() - last_actuation)
+                last_actuation = time.time_ns()"""
+        except Exception as e:
+            print("fucked")
         with pg.connect(f'dbname={dbname}  user={user} password={password} host={db_host} port=5432') as conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT * FROM tilt;")
-                cur.fetchone()
-                for record in cur:
-                    print(record)
+                sAll = sAll.split(',')
+                cur.execute("INSERT INTO soleniods (he, lng, lox, pv1, pv2, mvas) VALUES (%s, %s, %s, %s, %s, %s);", 
+                        sAll[0], sAll[1], sAll[2], sAll[3], sAll[4], sAll[5])
+                # cur.fetchone()
+                # for record in cur:
+                #     print(record)
 
 if __name__ == '__main__':
     start_loading()

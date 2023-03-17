70c3314850bb4d95df9442f6e4c4dc779a4a6bc9
Shah <aashay.as99@gmail.com>
2023-03-16 19:02:04 -0700

    Import code for Virtual Horizon

--- plugins/ucirp-ucirpvirtualhorizon-panel/src/components/SimplePanel.tsx
+++ plugins/ucirp-ucirpvirtualhorizon-panel/src/components/SimplePanel.tsx
@@ -29,8 +29,28 @@ const getStyles = () => {
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme2();
   const styles = useStyles2(getStyles);
+  const radius = 100;
+  const gap = options.gap;
+  // const angles_degrees = [Math.PI/4, 3*Math.PI/4, 5*Math.PI/4, 7*Math.PI/4, 0, Math.PI/2, Math.PI, 3*Math.PI/2]
+  let empAngleDegrees = [];
+  let angles_degrees = [];
+  let j = 0;
+  for(let x = 0;x<=360;x = x + gap) {
+	if(j % options.gapBetweenEmpMarks === 0) {
+		empAngleDegrees.push(x);
+	} else {
+  		angles_degrees.push(x);
+	}
+	j++;
+  }
+  let angles = angles_degrees.map((x) => x/180  * Math.PI);
+  let empAngleRadians = empAngleDegrees.map((x) => x/180 * Math.PI);
+  const currTilt = data.series
+  	.map((x) => x.fields.find((y) => y.type === 'number'))
+	.map((t) => t?.values.get(t.values.length - 1))[0];
+  const currTiltRadians = currTilt/180 * Math.PI;
   return (
-    <div
+   <div
       className={cx(
         styles.wrapper,
         css`
@@ -48,13 +68,25 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
       >
         <g>
-          <circle style={{ fill: theme.colors.primary.main }} r={100} />
+          <circle stroke={theme.colors.background.secondary} style={{ fill: theme.colors.background.secondary }} r={100} strokeWidth="4%"/>
+	  {
+	  angles.map((i) =>
+	  	<line x1={(radius+10)*Math.cos(i)} y1={(radius+10)*Math.sin(i)} x2={(radius-10)*Math.cos(i)} y2={(radius-10)*Math.sin(i)} stroke={theme.colors.secondary.text} strokeWidth="2"/>
+	  )
+	  }
+	  {
+	  empAngleRadians.map((i) => 
+	  	<line x1={(radius+10)*Math.cos(i)} y1={(radius+10)*Math.sin(i)} x2={(radius-10)*Math.cos(i)} y2={(radius-10)*Math.sin(i)} stroke={theme.colors.secondary.contrastText} strokeWidth="3"/>
+	  )	
+	  }
+  	<line x1={(radius-70)*Math.cos(Math.PI/2 + currTiltRadians)} y1={(radius-70)*Math.sin(Math.PI/2 + currTiltRadians)} x2={(radius-70)*Math.cos(3*Math.PI/2 + currTiltRadians)} y2={(radius-70)*Math.sin(3*Math.PI/2 + currTiltRadians)} stroke={theme.colors.text.primary} strokeWidth="2"/>
+  	<line x1={(radius-20)*Math.cos(Math.PI + currTiltRadians)} y1={(radius-20)*Math.sin(Math.PI + currTiltRadians)} x2={(radius-20)*Math.cos(currTiltRadians)} y2={(radius-20)*Math.sin(currTiltRadians)} stroke={theme.colors.text.primary} strokeWidth="2"/>
+          <circle stroke={theme.colors.border.strong} style={{ fill: theme.colors.primary.main }} r={7} />
         </g>
       </svg>
 
       <div className={styles.textBox}>
-        {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
-        <div>Text option value: {options.text}</div>
+	<div>Curr Tilt: {currTilt}</div>
       </div>
     </div>
   );

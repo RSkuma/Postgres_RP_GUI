2ee982b64f1c1a231f66e19b851c27620ab5d2c6
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2023-04-18 12:39:17 -0700

    Changes to fix bugs

--- data_loader/load_into_postgres.py
+++ data_loader/load_into_postgres.py
@@ -23,26 +23,21 @@ def start_loading(dbname, password, user, ecu_ip, db_host):
                 tc = [float(t) for t in tc.split(',') if t != '']
                 sAll = [int(s) for s in sAll.split(',') if s != '']
                 pt = [float(p) for p in pt.split(',') if p != '']
+                extra = float(extra[0])
 
-                print("TC: ", tc)
-                print(type(tc[0]))
                 print("PT: ", pt)
-                print(type(pt[0]))
-                print("Solenoid: ", sAll)
-                print(type(sAll[0]))
         except KeyboardInterrupt as k:
             conn.close()
             exit(0)
         except Exception as e:
-            click.secho("fucked", fg='red')
-            click.secho(e, fg='red')
+            click.secho(f"fucked {e}", fg='red')
         with conn.cursor() as cur:
             cur.execute("INSERT INTO solenoids (he, lng, lox, pv1, pv2, mvas) VALUES (%s, %s, %s, %s, %s, %s);", 
                     (sAll[0], sAll[1], sAll[2], sAll[3], sAll[4], sAll[5]))
-            cur.execute("INSERT INTO pt (pt1, pt2, pt3, pt4) VALUES (%s, %s, %s, %s);", pt[0], pt[1], pt[2], pt[3])
-            cur.execute("INSERT INTO tc (tc1, tc2, tc3, tc4) VALUES (%s, %s, %s, %s);", tc[0], tc[1], tc[2], tc[3])
+            cur.execute("INSERT INTO pt (pt1, pt2, pt3, pt4) VALUES (%s, %s, %s, %s);", (pt[3], pt[4], extra, pt[0]))
+            cur.execute("INSERT INTO tc (tc1, tc2, tc3, tc4) VALUES (%s, %s, %s, %s);", (tc[0], tc[1], tc[2], tc[3]))
         conn.commit()
 
 if __name__ == '__main__':
-        start_loading('postgres', 'postgres', 'postgres', '192.168.0.6', 'postgres')
+        start_loading('postgres', 'postgres', 'postgres', 'ecu', 'postgres')
 

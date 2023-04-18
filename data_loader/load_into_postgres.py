cdd487efde89d4c486e81380abfea775076d05cd
Shah <aashay.as99@gmail.com>
2023-04-18 12:51:25 -0700

    Change table for GSE data. Only push PT data for GSE

--- data_loader/load_into_postgres.py
+++ data_loader/load_into_postgres.py
@@ -33,10 +33,37 @@ def start_loading(dbname, password, user, ecu_ip, db_host):
             cur.execute("INSERT INTO tc (tc1, tc2, tc3, tc4) VALUES (%s, %s, %s, %s);", (tc[0], tc[1], tc[2], tc[3]))
         conn.commit()
 
+def gse_start_loading(dbname, password, user, ecu_ip, db_host):
+    udpReader = UdpReader(ecu_ip)
+    conn = pg.connect(f'dbname={dbname}  user={user} password={password} host={db_host} port=5432')
+
+    while True:
+        try:
+            data = udpReader.getData(f"sAll;tc;pt;")
+            if (data is not None):
+                data = str(data, encoding='utf-8')
+                sAll, tc, pt,  *extra = data.split(';')
+                print(sAll)
+                pt = [float(p) for p in pt.split(',') if p != '']
+                extra = float(extra[0])
+
+                print("PT: ", pt)
+            else:
+                print("No GSE found")
+                return
+        except KeyboardInterrupt as k:
+            conn.close()
+            exit(0)
+        except Exception as e:
+            click.secho(f"fucked {e}", fg='red')
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO gse_pt (pt1, pt2, pt3, pt4) VALUES (%s, %s, %s, %s);", (pt[3], pt[4], extra, pt[0]))
+        conn.commit()
+
 if __name__ == '__main__':
     t1 = Thread(target = start_loading, args = ('postgres', 'postgres', 'postgres', 'ecu', 'postgres'))
     t1.start()
-    t2 = Thread(target = start_loading, args = ('postgres', 'postgres', 'postgres', 'gse_ecu', 'postgres'))
+    t2 = Thread(target = gse_start_loading, args = ('postgres', 'postgres', 'postgres', 'gse_ecu', 'postgres'))
     t2.start()
     t1.join()
     t2.join()
